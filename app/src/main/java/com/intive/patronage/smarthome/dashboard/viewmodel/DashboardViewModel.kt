a05b28601d528aa6394d00dded67442bc20a614e
Daniel Pawiński <45467838+emkej0@users.noreply.github.com>
2020-03-04 21:54:27 +0100

    feature: added ViewModel and mock Sensor data class #PATRON2020-45

--- app/src/main/java/com/intive/patronage/smarthome/dashboard/viewmodel/DashboardViewModel.kt
+++ app/src/main/java/com/intive/patronage/smarthome/dashboard/viewmodel/DashboardViewModel.kt
@@ -1,6 +1,32 @@
 package com.intive.patronage.smarthome.dashboard.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.intive.patronage.smarthome.dashboard.model.SensorMock
+import io.reactivex.disposables.Disposable
 
 class DashboardViewModel : ViewModel() {
+
+    val items = MutableLiveData<List<SensorMock>>()
+    val error = MutableLiveData<Boolean>().apply { value = false }
+    private var sensorList: Disposable? = null
+
+
+    // tbd when API delivered(retrofit)
+//    init {
+//        itemsCall = matchResultService.fetchMatchResults()
+//            .subscribeOn(Schedulers.io())
+//            .observeOn(AndroidSchedulers.mainThread())
+//            .subscribe({
+//                items.value = it
+//            }, {
+//                error.value = true
+//            })
+//    }
+
+//    override fun onCleared() {
+//        super.onCleared()
+//        itemsCall?.dispose()
+//    }
+
 }
