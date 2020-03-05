940cfe2d49fd68b1167a4d7cdcc26cfbedd53490
MarcelIwanicki <iwanickimarcelgames@gmail.com>
2020-03-05 20:48:17 +0100

    feature: Created model classes for GET /dashboard

---
+++ app/src/main/java/com/intive/patronage/smarthome/model/WindowSensor.kt
@@ -0,0 +1,14 @@
+package com.intive.patronage.smarthome.model
+
+import com.squareup.moshi.Json
+
+enum class WindowSensorStatus {
+    OPEN,
+    CLOSE
+}
+
+data class WindowSensor(
+    @Json(name = "id") val id: Int,
+    @Json(name = "type") val type: String,
+    @Json(name = "status") val status: WindowSensorStatus
+)
