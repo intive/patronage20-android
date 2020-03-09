1e7eb0e8f93b0cfbc880c124e9ddafb2ed7cedd3
Konrad Onyszczuk <konrad_23_04_1992@o2.pl>
2020-03-09 18:27:50 +0100

    feature: add navigator to project

--- app/src/main/java/com/intive/patronage/smarthome/navigator/SplashScreenCoordinator.kt
+++ app/src/main/java/com/intive/patronage/smarthome/navigator/SplashScreenCoordinator.kt
@@ -3,10 +3,10 @@ package com.intive.patronage.smarthome.navigator
 import android.os.Bundle
 import com.intive.patronage.smarthome.MainActivity
 
-class SplashScreenCoordinator(private val navigator: Navigator, private val bundle: Bundle?) {
+class SplashScreenCoordinator(private val navigator: Navigator) {
 
     fun goToMainScreen() {
-        navigator.goToScreen(ActivityEvent(MainActivity::class.java, bundle))
+        navigator.goToScreen(ActivityEvent(MainActivity::class.java, null))
         navigator.close()
     }
 }
