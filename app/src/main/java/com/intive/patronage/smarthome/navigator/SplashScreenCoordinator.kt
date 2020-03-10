6eac8d5e651dcc0e4e77496d756d65506c7746bd
Konrad Onyszczuk <Konrad_23_04_1992@o2.pl>
2020-03-10 15:10:40 +0100

    feature: add navigator #PATRON2020#39 (#10)
    * feature: add navigator #PATRON2020-39

---
+++ app/src/main/java/com/intive/patronage/smarthome/navigator/SplashScreenCoordinator.kt
@@ -0,0 +1,12 @@
+package com.intive.patronage.smarthome.navigator
+
+import android.os.Bundle
+import com.intive.patronage.smarthome.MainActivity
+
+class SplashScreenCoordinator(private val navigator: Navigator) {
+
+    fun goToMainScreen() {
+        navigator.goToScreen(ActivityEvent(MainActivity::class.java))
+        navigator.close()
+    }
+}
