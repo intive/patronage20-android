d6fc1bd0bc80caa2e7a9067d357b2b3b10def263
Daniel Pawiński <45467838+emkej0@users.noreply.github.com>
2020-03-11 22:07:01 +0100

    Feature/patron2020 45 dashboard view and viewmodel (#20)
    * chore: added dashboard packages #PATRON2020-34
    
    * feature: added dashboard activity and appbar name #PATRON2020-34
    
    * feature: added dashboard fragment and corresponding layout #PATRON2020-34
    
    * fix: miscellaneous fixes #PATRON2020-45
    
    * refactor: changed DashboardActivity to SmartHomeActivity #PATRON2020-45
    
    * feature: added ViewModel and mock Sensor data class #PATRON2020-45
    
    * refactor: dashboard_activity changed to smart_home_activity #PATRON2020-45
    
    * refactor: deleted unused code #PATRON2020-45

---
+++ app/src/main/java/com/intive/patronage/smarthome/dashboard/view/SmartHomeActivity.kt
@@ -0,0 +1,19 @@
+package com.intive.patronage.smarthome.dashboard.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.intive.patronage.smarthome.R
+
+class SmartHomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.smart_home_activity)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        //navigator.quit()
+    }
+
+}
