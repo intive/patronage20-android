83e1c86fd2b42f607fca16ac50a08ed53e2754a9
Michał <michal.kucznerowicz@wp.pl>
2020-03-11 22:58:51 +0100

    chore: fixed conflicts with develop branch #PATRON2020-34

--- app/src/main/java/com/intive/patronage/smarthome/dashboard/view/SmartHomeActivity.kt
+++ app/src/main/java/com/intive/patronage/smarthome/dashboard/view/SmartHomeActivity.kt
@@ -3,12 +3,22 @@ package com.intive.patronage.smarthome.dashboard.view
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.intive.patronage.smarthome.R
+import kotlinx.android.synthetic.main.smart_home_activity.*
 
 class SmartHomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.smart_home_activity)
+
+        setSupportActionBar(toolbar)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.lights_details_fragment, LightsDetailsFragment()).addToBackStack(null)
+            .commit()
+
+        toolbar.setNavigationOnClickListener {
+            //TODO: implement navigator
+        }
     }
 
     override fun onBackPressed() {
