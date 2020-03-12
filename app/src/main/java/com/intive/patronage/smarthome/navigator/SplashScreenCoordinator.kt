1eed1ab00f5ff37862ceb100f883fabe227ffef9
Xsudo <dkiedrowicz@o2.pl>
2020-03-12 18:19:45 +0100

    feature : merged with develop #PATRON2020-29v2

--- app/src/main/java/com/intive/patronage/smarthome/navigator/SplashScreenCoordinator.kt
+++ app/src/main/java/com/intive/patronage/smarthome/navigator/SplashScreenCoordinator.kt
@@ -1,10 +1,12 @@
 package com.intive.patronage.smarthome.navigator
 
+import com.intive.patronage.smarthome.dashboard.view.SmartHomeActivity
+
 
 class SplashScreenCoordinator(private val navigator: Navigator) {
 
     fun goToMainScreen() {
-        // navigator.goToScreen(ActivityEvent(MainActivity::class.java))
+        navigator.goToScreen(ActivityEvent(SmartHomeActivity::class.java))
         navigator.close()
     }
 }
--- app/src/main/java/com/intive/patronage/smarthome/splashscreen/SmartHomeAlertDialog.kt
+++ app/src/main/java/com/intive/patronage/smarthome/splashscreen/SmartHomeAlertDialog.kt
@@ -2,14 +2,15 @@ package com.intive.patronage.smarthome.splashscreen
 
 import android.app.Activity
 import android.app.AlertDialog
+import com.intive.patronage.smarthome.R
 
 class SmartHomeAlertDialog(private val activity: Activity) {
 
     fun connectionError() {
         AlertDialog.Builder(activity)
-            .setTitle("Error")
-            .setMessage("Unable to connect with smart home")
-            .setNegativeButton("Quit") { _, _ -> activity.finish() }
+            .setTitle(R.string.error_title)
+            .setMessage(R.string.connection_error_message)
+            .setNegativeButton((R.string.quit)) { _, _ -> activity.finish() }
             .setCancelable(false)
             .show()
     }
