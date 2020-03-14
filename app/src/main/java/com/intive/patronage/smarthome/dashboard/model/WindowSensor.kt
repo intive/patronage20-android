5f5406fc092a0dd793306255713e15ee739bc91a
MarcelIwanicki <iwanickimarcelgames@gmail.com>
2020-03-14 10:54:28 +0100

    fix: changed windows sensor status from string to enum #PATRON2020-43

--- app/src/main/java/com/intive/patronage/smarthome/dashboard/model/WindowSensor.kt
+++ app/src/main/java/com/intive/patronage/smarthome/dashboard/model/WindowSensor.kt
@@ -2,8 +2,13 @@ package com.intive.patronage.smarthome.dashboard.model
 
 import com.squareup.moshi.Json
 
+enum class WindowSensorStatus {
+    @Json(name = "open") OPEN,
+    @Json(name = "close") CLOSED
+}
+
 data class WindowSensor(
     @Json(name = "id") val id: Int,
     @Json(name = "type") val type: String,
-    @Json(name = "status") val status: String
+    @Json(name = "status") val status: WindowSensorStatus
 )
