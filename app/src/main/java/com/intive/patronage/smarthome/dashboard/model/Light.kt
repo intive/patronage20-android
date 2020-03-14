1aaed2d6207cfa0a60e1737607c42db5ea5741c2
MarcelIwanicki <iwanickimarcelgames@gmail.com>
2020-03-14 10:54:28 +0100

    fix: renamed windowSensorStatus moshi json, renamed id to value in Light.kt #PATRON2020-43

--- app/src/main/java/com/intive/patronage/smarthome/dashboard/model/Light.kt
+++ app/src/main/java/com/intive/patronage/smarthome/dashboard/model/Light.kt
@@ -7,5 +7,5 @@ data class Light(
     @Json(name = "type") val type: String,
     @Json(name = "hue") val hue: Int,
     @Json(name = "saturation") val saturation: Int,
-    @Json(name = "id") val value: Int
+    @Json(name = "value") val value: Int
 )
