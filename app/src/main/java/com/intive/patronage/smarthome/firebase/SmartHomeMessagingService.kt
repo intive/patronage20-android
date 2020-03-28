6bf02fac6e8743c6c96e07e645c08ba7ab8f1912
MarcelIwanicki <iwanickimarcelgames@gmail.com>
2020-03-28 10:57:40 +0100

    feature: recieving push notifications from firebase #PATRON2020-118

---
+++ app/src/main/java/com/intive/patronage/smarthome/firebase/SmartHomeMessagingService.kt
@@ -0,0 +1,72 @@
+package com.intive.patronage.smarthome.firebase
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.intive.patronage.smarthome.R
+import com.intive.patronage.smarthome.SmartHomeApplication
+import java.util.*
+
+private const val TAG = "FCM_SERVICE"
+private const val CHANNEL_ID = "0"
+
+class SmartHomeMessagingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(message: RemoteMessage?) {
+        super.onMessageReceived(message)
+
+        message?.let {
+            sendNotification(it)
+            Log.d(TAG, "From: ${it.from}")
+            Log.d(TAG, "Notification: id: ${it.messageId}, body: ${it.notification.body}")
+        }
+    }
+
+    private fun sendNotification(message: RemoteMessage) {
+        createNotificationChannel()
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_home_white_24dp)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(getPendingIntent())
+            .setStyle(
+                NotificationCompat.BigTextStyle().bigText(message.notification.body)
+                    .setBigContentTitle(message.notification.title)
+            )
+            .setAutoCancel(true)
+
+        with(NotificationManagerCompat.from(this)) {
+            notify(UUID.randomUUID().variant(), builder.build())
+        }
+
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun getPendingIntent(): PendingIntent {
+        val intent = Intent(this, SmartHomeApplication::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        return PendingIntent.getActivity(this, 0, intent, 0)
+    }
+}
