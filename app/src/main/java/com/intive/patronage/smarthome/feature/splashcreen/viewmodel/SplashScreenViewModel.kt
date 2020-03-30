4cbe4fe57407e7e32c789cc81a49854aeec01d90
daniel <emkejowsky@gmail.com>
2020-03-30 19:43:48 +0200

    fix: changed how splash screen handles delay while downloading data #PATRON2020-133

--- app/src/main/java/com/intive/patronage/smarthome/feature/splashcreen/viewmodel/SplashScreenViewModel.kt
+++ app/src/main/java/com/intive/patronage/smarthome/feature/splashcreen/viewmodel/SplashScreenViewModel.kt
@@ -8,11 +8,13 @@ import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
+import io.reactivex.Observable
+import io.reactivex.rxkotlin.zipWith
 
 
 class SplashScreenViewModel(private val dashboardService: DashboardService) : ViewModel() {
 
-    private val minWaitTime = 0L
+    private val minWaitTime = 5L
     private val maxWaitTime = 30L
 
     private var dashboard = MutableLiveData<Dashboard>()
@@ -21,14 +23,14 @@ class SplashScreenViewModel(private val dashboardService: DashboardService) : Vi
     private var dashboardCall: Disposable? = null
 
     fun getSensors() {
+        val timer = Observable.timer(minWaitTime, TimeUnit.SECONDS)
 
-        dashboardCall = dashboardService.getDashboard().toObservable()
-            .delay(minWaitTime, TimeUnit.SECONDS, true)
+        dashboardCall = dashboardService.getDashboard().toObservable().zipWith(timer)
             .timeout(maxWaitTime, TimeUnit.SECONDS)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
-                dashboard.value = it
+                dashboard.value = it.first
             }, { error.value = true }, { complete.value = true })
     }
 
