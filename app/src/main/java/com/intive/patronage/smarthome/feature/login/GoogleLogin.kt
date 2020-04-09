a83910730e2281f3800483e8527144f3fa79e79c
Konrad Onyszczuk <konrad_23_04_1992@o2.pl>
2020-04-09 12:49:17 +0200

    feature: add login with google #PATRON2020-120

--- app/src/main/java/com/intive/patronage/smarthome/feature/login/GoogleLogin.kt
+++ app/src/main/java/com/intive/patronage/smarthome/feature/login/GoogleLogin.kt
@@ -1,11 +1,15 @@
 package com.intive.patronage.smarthome.feature.login
 
+import android.content.Intent
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.intive.patronage.smarthome.R
@@ -32,7 +36,7 @@ class GoogleLogin(private val appCompatActivity: AppCompatActivity, private val 
         mAuth = FirebaseAuth.getInstance()
     }
 
-    fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
         val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
         mAuth.signInWithCredential(credential).addOnCompleteListener {
             if (it.isSuccessful) {
@@ -48,7 +52,6 @@ class GoogleLogin(private val appCompatActivity: AppCompatActivity, private val 
     }
 
     fun signOut() {
-
         mAuth.signOut()
 
         mGoogleSignInClient.signOut().addOnCompleteListener {
@@ -60,6 +63,20 @@ class GoogleLogin(private val appCompatActivity: AppCompatActivity, private val 
         val currentUser = mAuth.currentUser
         if (currentUser != null) {
             loginCoordinator.goToMainScreen()
+        }
+    }
+
+    fun accountVerification(requestCode: Int, data: Intent?) {
+        if (requestCode == RC_SIGN_IN) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
+                Toast.makeText(appCompatActivity, "Google sign in is successfully", Toast.LENGTH_SHORT).show()
+            } catch (e: ApiException) {
+                Toast.makeText(appCompatActivity, "Google sign in failed", Toast.LENGTH_SHORT).show()
+                Log.w(appCompatActivity.getString(R.string.login), "Google sign in failed", e)
+            }
         }
     }
 }
--- app/src/main/java/com/intive/patronage/smarthome/feature/login/LoginActivity.kt
+++ app/src/main/java/com/intive/patronage/smarthome/feature/login/LoginActivity.kt
@@ -2,13 +2,7 @@ package com.intive.patronage.smarthome.feature.login
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
 import com.intive.patronage.smarthome.R
 import org.koin.android.ext.android.inject
 import org.koin.core.parameter.parametersOf
@@ -36,16 +30,6 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == googleLogin.RC_SIGN_IN) {
-            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
-                val account = task.getResult(ApiException::class.java)
-                googleLogin.firebaseAuthWithGoogle(account!!)
-                Toast.makeText(this, "Google sign in is successfully", Toast.LENGTH_SHORT).show()
-            } catch (e: ApiException) {
-                Toast.makeText(this, "Google sign in failed", Toast.LENGTH_SHORT).show()
-                Log.w(getString(R.string.login), "Google sign in failed", e)
-            }
-        }
+        googleLogin.accountVerification(requestCode, data)
     }
 }
