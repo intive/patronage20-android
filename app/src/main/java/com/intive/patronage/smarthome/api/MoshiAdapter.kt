ccbdd714b29ecf4e119956506b52a992d687dc97
Vezrix <Vezrix@gmail.com>
2020-04-20 18:28:32 +0200

    fix: fixed values in hvac adapter, deleted mock interceptor #PATRON2020-247

--- app/src/main/java/com/intive/patronage/smarthome/api/MoshiAdapter.kt
+++ app/src/main/java/com/intive/patronage/smarthome/api/MoshiAdapter.kt
@@ -5,11 +5,12 @@ import com.intive.patronage.smarthome.feature.dashboard.model.Light
 import com.intive.patronage.smarthome.feature.dashboard.model.TemperatureSensor
 import com.intive.patronage.smarthome.feature.dashboard.model.WindowBlind
 import com.squareup.moshi.FromJson
+import kotlin.random.Random
 
 class TemperatureAdapter {
     @FromJson
     fun fromJson(temperature: TemperatureSensor): TemperatureSensor {
-        return TemperatureSensor(temperature.id, temperature.type, 10, temperature.mapPosition)
+        return TemperatureSensor(temperature.id, temperature.type, Random.nextInt(31) + 10, temperature.mapPosition)
     }
 }
 
@@ -22,7 +23,7 @@ class HVACRoomAdapter {
             100,
             200,
             10,
-            hvacRoom.temperatureSensorId,
+            1,
             hvacRoom.windowSensorIds,
             hvacRoom.mapPosition
         )
