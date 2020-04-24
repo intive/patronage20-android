1465070d636c970eba20df7026c37c7ad7e98f10
Michał <michal.kucznerowicz@wp.pl>
2020-04-25 00:25:40 +0200

    chore: deleted mock interceptor #PATRON2020-239

--- app/src/main/java/com/intive/patronage/smarthome/di/NetworkApiModule.kt
+++ app/src/main/java/com/intive/patronage/smarthome/di/NetworkApiModule.kt
@@ -1,7 +1,6 @@
 package com.intive.patronage.smarthome.di
 
 import com.intive.patronage.smarthome.feature.dashboard.model.api.authorization.AuthorizationInterceptor
-import com.intive.patronage.smarthome.feature.dashboard.model.api.mock.MockInterceptor
 import okhttp3.OkHttpClient
 import org.koin.dsl.module
 import java.util.concurrent.TimeUnit
@@ -13,7 +12,6 @@ val networkApiModule = module {
             .callTimeout(120, TimeUnit.SECONDS)
             .connectTimeout(120, TimeUnit.SECONDS)
             .addInterceptor(AuthorizationInterceptor())
-            .addInterceptor(MockInterceptor())
             .cache(null)
             .build()
     }
