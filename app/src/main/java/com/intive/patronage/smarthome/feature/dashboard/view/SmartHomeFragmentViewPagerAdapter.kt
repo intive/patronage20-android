768ae070634ed10ebd8b950f5c38b3b8e7fb7591
daniel <emkejowsky@gmail.com>
2020-04-26 17:58:05 +0200

    feature: migrated to vp2 #PATRON2020-131

--- app/src/main/java/com/intive/patronage/smarthome/feature/dashboard/view/SmartHomeFragmentViewPagerAdapter.kt
+++ app/src/main/java/com/intive/patronage/smarthome/feature/dashboard/view/SmartHomeFragmentViewPagerAdapter.kt
@@ -2,31 +2,37 @@ package com.intive.patronage.smarthome.feature.dashboard.view
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.intive.patronage.smarthome.feature.home.view.HomeFragment
 
 
-class SmartHomeFragmentViewPagerAdapter(
-    fragmentManager: FragmentManager,
-    private val titleMap: Map<Int, String>
-) :
-    FragmentStatePagerAdapter(fragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+class SmartHomeFragmentViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+    FragmentStateAdapter(fragmentManager, lifecycle) {
 
-
-    override fun getItem(position: Int): Fragment {
+    override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> DashboardFragment()
             else -> HomeFragment()
         }
     }
 
-    override fun getCount(): Int = 2
+    override fun getItemCount() = 2
 
-    override fun getPageTitle(position: Int): CharSequence? {
-        return when (position) {
-            // add to strings.xml
-            0 -> titleMap[0]
-            else -> titleMap[1]
-        }
-    }
+//    override fun getItem(position: Int): Fragment {
+//        return when (position) {
+//            0 -> DashboardFragment()
+//            else -> HomeFragment()
+//        }
+//    }
+//
+//    override fun getCount(): Int = 2
+//
+//    override fun getPageTitle(position: Int): CharSequence? {
+//        return when (position) {
+//            // add to strings.xml
+//            0 -> titleMap[0]
+//            else -> titleMap[1]
+//        }
+//    }
 }
