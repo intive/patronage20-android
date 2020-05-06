9a79f4af7977c72093e4c8e0cf2ace05dde3ec5a
Konrad Onyszczuk <konrad_23_04_1992@o2.pl>
2020-05-06 13:20:46 +0200

    fix: change version viewpager2

--- app/src/main/java/com/intive/patronage/smarthome/feature/dashboard/view/SmartHomeFragmentViewPagerAdapter.kt
+++ app/src/main/java/com/intive/patronage/smarthome/feature/dashboard/view/SmartHomeFragmentViewPagerAdapter.kt
@@ -9,6 +9,8 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 class SmartHomeFragmentViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
     FragmentStateAdapter(fragmentManager, lifecycle) {
 
+    private val numberTabsInTabLayout = 2
+
     override fun createFragment(position: Int): Fragment {
         return when (position) {
             0 -> DashboardFragment()
@@ -16,5 +18,5 @@ class SmartHomeFragmentViewPagerAdapter(fragmentManager: FragmentManager, lifecy
         }
     }
 
-    override fun getItemCount() = 2
+    override fun getItemCount() = numberTabsInTabLayout
 }
