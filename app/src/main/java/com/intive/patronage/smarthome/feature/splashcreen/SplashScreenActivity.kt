80fa76017e132ac62e37172bae967a739c2eb21e
Michał <michal.kucznerowicz@wp.pl>
2020-05-17 10:32:29 +0200

    feature: simplified splash screen animation #PATRON2020-379

--- app/src/main/java/com/intive/patronage/smarthome/feature/splashcreen/SplashScreenActivity.kt
+++ app/src/main/java/com/intive/patronage/smarthome/feature/splashcreen/SplashScreenActivity.kt
@@ -6,10 +6,9 @@ import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.view.animation.AnimationUtils
-import android.widget.ImageView
 import android.widget.ProgressBar
-import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import com.intive.patronage.smarthome.R
 import com.intive.patronage.smarthome.common.DeeplinkService
@@ -18,12 +17,10 @@ import com.intive.patronage.smarthome.feature.splashcreen.animation.ProgressBarA
 import com.intive.patronage.smarthome.feature.splashcreen.viewmodel.SplashScreenViewModel
 import com.intive.patronage.smarthome.navigator.SplashScreenCoordinator
 import com.intive.patronage.smarthome.notifications.service.SmartHomeNotificationsService
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
 import org.koin.core.parameter.parametersOf
-
-private const val SLIDE_DURATION = 1000L
-private const val FADE_DURATION = 2000L
 
 class SplashScreenActivity : AppCompatActivity() {
 
@@ -60,27 +57,15 @@ class SplashScreenActivity : AppCompatActivity() {
     }
 
     private fun startSlideInAnimation() {
-        val icon = findViewById<ImageView>(R.id.splashScreenIcon)
-        val iconLayout = findViewById<RelativeLayout>(R.id.iconLayout)
-        val logo = findViewById<ImageView>(R.id.splashScreenLogo)
-
-        icon.foreground = resources.getDrawable(R.drawable.wifi_animation)
-        val wifiAnimation = icon.foreground as AnimationDrawable
+        splashScreenIcon.foreground = ContextCompat.getDrawable(this, R.drawable.wifi_animation)
+        val wifiAnimation = splashScreenIcon.foreground as AnimationDrawable
         wifiAnimation.start()
 
-        val fromBottom = AnimationUtils.loadAnimation(this, R.anim.from_bottom_splash)
-        fromBottom.duration = SLIDE_DURATION
+        val fromBottom = AnimationUtils.loadAnimation(this, R.anim.from_bottom_splash_screen)
+        val fromTop = AnimationUtils.loadAnimation(this, R.anim.from_top_splash_screen)
 
-        val fromTop = AnimationUtils.loadAnimation(this, R.anim.from_top_splash)
-        fromTop.duration = SLIDE_DURATION
-
-        icon.animation = fromBottom
-        iconLayout.animation = fromBottom
-        logo.animation = fromTop
-
-        icon.animate().alpha(1f).duration = FADE_DURATION
-        iconLayout.animate().alpha(1f).duration = FADE_DURATION
-        logo.animate().alpha(1f).duration = FADE_DURATION
+        iconLayout.startAnimation(fromBottom)
+        splashScreenLogo.startAnimation(fromTop)
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
@@ -124,11 +109,11 @@ class SplashScreenActivity : AppCompatActivity() {
 
     private fun fullScreen() {
         window.decorView.systemUiVisibility = (
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        or View.SYSTEM_UI_FLAG_FULLSCREEN)
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
     }
 }
